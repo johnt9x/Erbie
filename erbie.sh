256b31245d9a0699522a0762cd620b88c1f1b206
johnt9x <40466326+johnt9x@users.noreply.github.com>
2023-08-15 22:06:25 +0700

    Update erbie.sh

--- erbie.sh
+++ erbie.sh
@@ -1,45 +1,39 @@
 #!/bin/bash
-. ~/.bashrc
-mkdir -p .erbie/erbie
 
-#update package
+# Update system and install build tools
 sudo apt update && sudo apt list --upgradable && sudo apt upgrade -y
-
-#install library
 sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git make ncdu net-tools -y
 
-#install go
-ver="1.20.2" && \
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
-sudo rm -rf /usr/local/go && \
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
-rm "go$ver.linux-amd64.tar.gz" && \
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
-source $HOME/.bash_profile && \
+# Install go
+ver="1.20.2"
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile
+source $HOME/.bash_profile
 go version
 
-#install binary
+# Install binary & build binary
+mkdir -p .erbie/erbie
 cd $HOME
 git clone https://github.com/erbieio/erbie
 cd erbie
 git checkout v0.14.4
-sleep 5
-
-#build binary
-cd erbie
 go build -o erbie cmd/erbie/main.go
-mv erbie /usr/local/bin
+sudo mv erbie /usr/local/bin
 
-#create service
-tee /etc/systemd/system/erbied.service > /dev/null <<EOF
+# Create & start service
+sudo tee /etc/systemd/system/erbied.service > /dev/null <<EOF
 [Unit]
 Description=erbie
 After=online.target
+
 [Service]
 Type=simple
 User=$USER
 WorkingDirectory=$HOME
-ExecStart= /usr/local/bin/erbie \
+ExecStart=/usr/local/bin/erbie \
   --datadir $HOME/.erbie \
   --devnet \
   --identity johnt9x \
@@ -57,11 +51,11 @@ ExecStart= /usr/local/bin/erbie \
 Restart=on-failure
 RestartSec=5
 LimitNOFILE=4096
+
 [Install]
 WantedBy=multi-user.target
 EOF
 
-#start service
 sudo systemctl daemon-reload
 sudo systemctl enable erbied
 sudo systemctl start erbied
