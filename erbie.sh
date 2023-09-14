ed09320fbb708e0d81c36e1baa1710076045c8a0
johnt9x <40466326+johnt9x@users.noreply.github.com>
2023-09-14 15:59:33 +0700

    Update erbie.sh

--- erbie.sh
+++ erbie.sh
@@ -19,7 +19,7 @@ mkdir -p .erbie/erbie
 cd $HOME
 git clone https://github.com/erbieio/erbie
 cd erbie
-git checkout v0.14.5
+git checkout v0.15.0
 go build -o erbie cmd/erbie/main.go
 sudo mv erbie /usr/local/bin
 
